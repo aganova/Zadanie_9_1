cb50d7bbe9620fb4f4311b3881c51c3062dd3c20
Agnieszka Nowacka <aguo84@gmail.com>
2018-01-10 23:17:14 +0100

    Add html and js files

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+function getTriangleArea(a, h) {
+	if ((a > 0) && (h > 0)) {
+		return (a * h / 2);
+	} else {
+		return 'Incorrect data';
+	}
+}
+
+console.log(getTriangleArea(10, 6));
+
+var triangle1Area = getTriangleArea(3.5, 50);
+console.log(triangle1Area);
